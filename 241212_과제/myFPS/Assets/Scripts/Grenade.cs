08e1d32689da55d664451b6aa1bbff43ab356e12
Jiyun-Juli-Kim <juliyooni@gmail.com>
2024-12-13 18:20:16 +0900

    저장용

--- 241212_과제/myFPS/Assets/Scripts/Grenade.cs
+++ 241212_과제/myFPS/Assets/Scripts/Grenade.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject _grenadePrefab;
+    [SerializeField] private Transform  _throwPoint;
+    [SerializeField] private float _throwForce;
+    private bool isDetected = false;
+    private GameObject _target;
 
-    // Update is called once per frame
+    private Rigidbody _grenadeRb;
+
     void Update()
     {
-        
+        Throw();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            Destroy(gameObject, 3f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            isDetected = true;
+            _target = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            isDetected = false;
+        }
+    }
+
+    private void Throw()
+    {
+        if (Input.GetMouseButtonDown(0))
+        { 
+            GameObject grenade = Instantiate(_grenadePrefab, _throwPoint.position, _throwPoint.rotation);
+            grenade.GetComponent<Rigidbody>().AddForce(_throwPoint.forward * _throwForce, ForceMode.Impulse);
+        }
+    }
+
+    private void Explode()
+    {
+        Destroy(gameObject);
+        if (isDetected)
+        {
+            _target.GetComponent<Enemy>().GetDamage(20f);
+        }
     }
 }
